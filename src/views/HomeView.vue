4a4259c97f0092c234f7b620adff8e6b308111e7
LukaTrunic <lukatrunic@gmail.com>
2025-04-26 01:10:03 +0200

    Added kitchen sink

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,30 @@ import { ref } from 'vue';
 
 const books = ref<BookModel[]>()
 BookService.getBooks()
-      .then(rsp => books.value = rsp.data)
+  .then(rsp => books.value = rsp.data)
 
 </script>
 
 <template>
+  <div class="wrapper">
+    <div class="card" style="width: 18rem;" v-for="b of books" :key="b.id" >
+      <img src="" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+          content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
   <ul>
     <li v-for="b of books" :key="b.id">{{ b.author }} {{ b.category }}</li>
   </ul>
