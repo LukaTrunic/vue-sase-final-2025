f0dc73c35f842f773791d6ee970652ae4d00e863
LukaTrunic <lukatrunic@gmail.com>
2025-04-28 10:48:34 +0200

    Improved Loading

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="mx-auto text-center">
-        <h1><i class="fa-solid fa-spinner"></i></h1>
-        <h3>Loading data...</h3>
-        <p>Please wait while we are loading your data</p>
+    <div class="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
+        <i class="fa-solid fa-spinner fa-spin text-5xl text-blue-500"></i>
+        <h2 class="text-2xl font-semibold">Loading...</h2>
+        <p class="text-gray-500">Hang tight, we’re preparing your data.</p>
     </div>
 </template>
